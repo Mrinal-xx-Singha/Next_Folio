d72041239bdd6bfc3b8e900a59d98586be752f32
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2024-08-05 21:19:22 +0530

    Personal website

---
+++ components/Footer.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] pb-10" id="contact">
+
+      {/* content */}
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to take ?<span className="text-purple ">Your</span> digital
+          presence to the next level
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me to today and let&apos;s discuss how i can help you
+          achieve your goals
+        </p>
+
+        <a href="mailto:mrinalsingha17@gmail.com">
+          <MagicButton
+            title="Lets's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Mrinal Singha
+        </p>
+      </div>
+
+      <div className="flex  items-center justify-center md:gap-3 lg:gap-6 md:mb-5">
+        {socialMedia.map((profile) => (
+          <div
+            key={profile.id}
+            className="mt-6 w-10 h-10 cursor-pointer flex justify-center  backdrop-blur-lg backdrop-filter saturate-100 bg-opacity-75 bg-black-200
+            border-black-300 border rounded-lg items-center
+            "
+          >
+            <Link  href={profile.link}>
+            <img src={profile.img} alt={profile.id} width={20} height={20} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,189 @@
+import Link from "next/link";
+
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Approach", link: "#approach" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Delice",
+    des: "Engineered a sophisticated FoodRecipe MultiPage WebApplication leveraging React.js for optimal UI/UX design,",
+    img: "/delice.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://react-redux-teal.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Mrinal Tube",
+    des: "A video searching and playing web application which lets you see videos of creators.",
+    img: "/Youtube.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://main--remarkable-choux-7fefb8.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Mrinals Metaverse",
+    des: "Metaverse is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the madness of the metaverseof today, using only VR devices you can easily explore the metaverse world you want, turn your dreams into reality.",
+    img: "/Metaversus.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://64d907c3aee17330c78eff62--peppy-chimera-b2544c.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "Netflix UI",
+    des: "Recreated the Netflix website. ",
+    img: "/Netflix.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://netflix-ui-green.vercel.app/",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - JSM Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Mrinal-xx-Singha"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://twitter.com/Mrinal__Singha"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/mrinal-singha/"
+  },
+];
