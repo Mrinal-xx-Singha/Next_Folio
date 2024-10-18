cef2409675cc641f89cc86b0fe6f0b8f7c878a46
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2024-10-18 21:41:13 +0530

    minor adjustments

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -9,7 +9,6 @@ import { useState } from "react";
 import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
-import { setTimeout } from "timers";
 
 export const BentoGrid = ({
   className,
@@ -49,7 +48,7 @@ export const BentoGridItem = ({
   img?: string;
   imgClassName?: string;
   titleClassName?: string;
-  spareImg: string;
+  spareImg: string; // Ensure this is passed correctly as it's required
 }) => {
   const [copied, setCopied] = useState(false);
   const [copiedResume, setCopiedResume] = useState(false);
@@ -57,9 +56,9 @@ export const BentoGridItem = ({
   const resumeLink =
     "https://drive.google.com/file/d/1p6X7b03YdE3o459_SoTbH7-Mvwm1Wk5c/view?usp=drive_link";
 
-  const handleCopyResume = (event) => {
-    // Prevent the default button behavior
-    event.preventDefault();
+  const handleCopyResume = () => {
+    const resumeLink =
+      "https://drive.google.com/file/d/1p6X7b03YdE3o459_SoTbH7-Mvwm1Wk5c/view?usp=drive_link";
 
     // Open the resume link in a new tab
     window.open(resumeLink, "_blank");
@@ -77,6 +76,7 @@ export const BentoGridItem = ({
         console.log("Failed to copy: ", err);
       });
   };
+
   const handleCopy = () => {
     navigator.clipboard.writeText("mrinalsingha17@gmail.com");
     setCopied(true);
@@ -85,7 +85,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        " row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -94,7 +94,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(10,10,10,1) 0%, rgba(30,30,30,1) 26%, rgba(15,15,15,1) 100%)", // Updated black gradient
       }}
     >
-      {/* img dives */}
+      {/* img div */}
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
@@ -146,7 +146,6 @@ export const BentoGridItem = ({
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
               {/* tech stack */}
-
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-2">
                 {["React.js", "Next.js", "Typescript"].map((item, i) => (
                   <span
@@ -156,16 +155,12 @@ export const BentoGridItem = ({
                     {item}
                   </span>
                 ))}
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#121111]"></span>
               </div>
               <div className="flex flex-col md:gap-3 gap-3 lg:gap-2">
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#121111]"></span>
-
                 {["Express", "MongoDB", "Javascript"].map((item, i) => (
                   <span
                     key={i}
-                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#121111]"
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#121111]"
                   >
                     {item}
                   </span>
@@ -180,7 +175,6 @@ export const BentoGridItem = ({
                 <Lottie
                   options={{
                     loop: copied,
-
                     autoplay: copied,
                     animationData: animationData,
                     rendererSettings: {
